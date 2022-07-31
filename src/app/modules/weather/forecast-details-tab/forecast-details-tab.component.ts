866132baefad5a7d157cc9f9e7b10490a4a9079f
PawelRadawiec <pawel.radaw@gmail.com>
2022-07-31 09:56:53 +0200

    tab details - temp mode

--- src/app/modules/weather/forecast-details-tab/forecast-details-tab.component.ts
+++ src/app/modules/weather/forecast-details-tab/forecast-details-tab.component.ts
@@ -1,17 +1,18 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ForecastDay } from 'src/app/models/weather-details.model';
+import { ForecastCommonTabService, TempMode } from 'src/app/services/forecast-common-tab.service';
 
 @Component({
   selector: 'app-forecast-details-tab',
   templateUrl: './forecast-details-tab.component.html',
-  styleUrls: ['./forecast-details-tab.component.scss']
+  styleUrls: ['./forecast-details-tab.component.scss'],
 })
 export class ForecastDetailsTabComponent implements OnInit {
   @Input() data: ForecastDay;
 
-  constructor() { }
+  tempMode = TempMode;
 
-  ngOnInit() {
-  }
+  constructor(public commonTabService: ForecastCommonTabService) {}
 
+  ngOnInit() {}
 }
